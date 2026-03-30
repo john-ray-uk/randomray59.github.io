d7a3c9ec3e2189841297ed9a6935efbbc847b855
John Ray <photographybyjohnray@gmail.com>
2026-03-30 17:58:14 +0100

    Alicante day one page and photos
    This is a big commit, please work properly.

---
+++ scripts/alicante-one-background.js
@@ -0,0 +1,10 @@
+    // As the user scrolls,the background opacity is updated (0 at scrollY = 0 to 1 at scrollY = 1500)
+    // This doesn't seem to work on older browsers. Presumably the HTML tags I use here are new or something.
+    window.addEventListener('scroll', () => {
+      let scrollY = window.scrollY;
+      let fraction = Math.min(scrollY / 1500, 1);
+      document.body.style.setProperty('--bg-opacity', fraction);
+      let fraction2 = (50 + 50 * Math.min(scrollY / 500, 1)) + '%';
+      document.body.style.setProperty('--NavBarmovement', fraction2);
+    });
+    document.body.style.setProperty('--bg-image', "url('/images/alicante/day-one/AlicanteBackground1.JPG')");
